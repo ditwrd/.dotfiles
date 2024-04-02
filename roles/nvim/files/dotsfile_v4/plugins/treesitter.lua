1b943b37ef1226189159ce97e583fec454ef3f25
Aditya Wardianto <aditya.wardianto11@gmail.com>
2024-04-02 11:18:41 +0700

    .

--- roles/nvim/files/dotsfile_v4/plugins/treesitter.lua
+++ roles/nvim/files/dotsfile_v4/plugins/treesitter.lua
@@ -4,17 +4,17 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "python",
-      "terraform",
-      "javascript",
-      "rust",
-      "dockerfile",
-      "go",
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	opts = function(_, opts)
+		-- add more things to the ensure_installed table protecting against community packs modifying it
+		opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+			"lua",
+			"python",
+			"terraform",
+			"javascript",
+			"rust",
+			"dockerfile",
+			"go",
+		})
+	end,
 }
