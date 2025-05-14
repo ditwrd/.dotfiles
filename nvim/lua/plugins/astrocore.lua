7d251dbbbe4d63cd2a6cd8ca2ecc8aec29e0e8b5
Aditya W <aditya@cube.asia>
2025-05-14 16:38:30 +0700

    astronvim v4 -> v5

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -1,3 +1,119 @@
+-- -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--
+-- -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
+-- -- Configuration documentation can be found with `:h astrocore`
+-- -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+-- --       as this provides autocomplete and documentation while editing
+--
+-- ---@type LazySpec
+-- return {
+-- 	"AstroNvim/astrocore",
+-- 	---@type AstroCoreOpts
+-- 	opts = {
+-- 		-- Configure core features of AstroNvim
+-- 		features = {
+-- 			large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+-- 			autopairs = true,                              -- enable autopairs at start
+-- 			cmp = true,                                    -- enable completion at start
+-- 			diagnostics_mode = 3,                          -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+-- 			highlighturl = true,                           -- highlight URLs at start
+-- 			notifications = true,                          -- enable notifications at start
+-- 		},
+-- 		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+-- 		diagnostics = {
+-- 			virtual_text = true,
+-- 			underline = true,
+-- 		},
+-- 		-- vim options can be configured here
+-- 		options = {
+-- 			opt = {
+-- 				-- set to true or false etc.
+-- 				relativenumber = true, -- sets vim.opt.relativenumber
+-- 				number = true,     -- sets vim.opt.number
+-- 				spell = false,     -- sets vim.opt.spell
+-- 				signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+-- 				wrap = false,      -- sets vim.opt.wrap
+-- 			},
+-- 			g = {
+-- 				autoformat_enabled = true,   -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+-- 				cmp_enabled = true,          -- enable completion at start
+-- 				autopairs_enabled = true,    -- enable autopairs at start
+-- 				diagnostics_mode = 3,        -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+-- 				icons_enabled = true,        -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+-- 				ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+-- 				move_key_modifier = "C",
+-- 				move_key_modifier_visualmode = "C",
+-- 			},
+-- 		},
+-- 		-- Mappings can be configured through AstroCore as well.
+-- 		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+-- 		mappings = {
+-- 			n = {
+-- 				-- second key is the lefthand side of the map
+-- 				-- mappings seen under group name "Buffer"
+-- 				--
+-- 				["-"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
+-- 				["\\"] = false,
+-- 				["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+-- 				["<Leader>bD"] = {
+-- 					function()
+-- 						require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+-- 							require("astronvim.utils.buffer").close(bufnr)
+-- 						end)
+-- 					end,
+-- 					desc = "Pick to close",
+-- 				},
+-- 				["<Leader>b"] = { name = "Buffers" },
+-- 				["<Leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
+-- 				["<Leader>re"] = {
+-- 					function()
+-- 						require("nvim-python-repl").send_statement_definition()
+-- 					end,
+-- 					desc = "Send semantic unit to REPL",
+-- 				},
+-- 				["<Leader>B"] = { name = "Bounding Box" },
+-- 				["<Leader>Bb"] = {
+-- 					"<Cmd>CBccbox<CR>",
+-- 					desc = "Title box",
+-- 				},
+-- 				["<Leader>Bt"] = {
+-- 					"<Cmd>CBllline<CR>",
+-- 					desc = "Title line",
+-- 				},
+-- 				["<Leader>Bl"] = {
+-- 					"<Cmd>CBline<CR>",
+-- 					desc = "Comment line",
+-- 				},
+-- 			},
+-- 			t = {
+-- 				-- setting a mapping to false will disable it
+-- 				-- ["<esc>"] = false,
+-- 			},
+-- 			v = {
+-- 				["<Leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
+-- 				["<Leader>re"] = {
+-- 					function()
+-- 						require("nvim-python-repl").send_visual_to_repl()
+-- 					end,
+-- 					desc = "Send visual selection to REPL",
+-- 				},
+-- 				["<Leader>B"] = { name = "Bounding Box" },
+-- 				["<Leader>Bb"] = {
+-- 					"<Cmd>CBccbox<CR>",
+-- 					desc = "Title box",
+-- 				},
+-- 				["<Leader>Bt"] = {
+-- 					"<Cmd>CBllline<CR>",
+-- 					desc = "Title line",
+-- 				},
+-- 				["<Leader>Bl"] = {
+-- 					"<Cmd>CBline<CR>",
+-- 					desc = "Comment line",
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- }
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
@@ -7,110 +123,141 @@
 
 ---@type LazySpec
 return {
-	"AstroNvim/astrocore",
-	---@type AstroCoreOpts
-	opts = {
-		-- Configure core features of AstroNvim
-		features = {
-			large_buf = { size = 1024 * 500, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-			autopairs = true,                              -- enable autopairs at start
-			cmp = true,                                    -- enable completion at start
-			diagnostics_mode = 3,                          -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-			highlighturl = true,                           -- highlight URLs at start
-			notifications = true,                          -- enable notifications at start
-		},
-		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-		diagnostics = {
-			virtual_text = true,
-			underline = true,
-		},
-		-- vim options can be configured here
-		options = {
-			opt = {
-				-- set to true or false etc.
-				relativenumber = true, -- sets vim.opt.relativenumber
-				number = true,     -- sets vim.opt.number
-				spell = false,     -- sets vim.opt.spell
-				signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-				wrap = false,      -- sets vim.opt.wrap
-			},
-			g = {
-				autoformat_enabled = true,   -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-				cmp_enabled = true,          -- enable completion at start
-				autopairs_enabled = true,    -- enable autopairs at start
-				diagnostics_mode = 3,        -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-				icons_enabled = true,        -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-				ui_notifications_enabled = true, -- disable notifications when toggling UI elements
-				move_key_modifier = "C",
-				move_key_modifier_visualmode = "C",
-			},
-		},
-		-- Mappings can be configured through AstroCore as well.
-		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-		mappings = {
-			n = {
-				-- second key is the lefthand side of the map
-				-- mappings seen under group name "Buffer"
-				--
-				["-"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
-				["\\"] = false,
-				["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-				["<Leader>bD"] = {
-					function()
-						require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
-							require("astronvim.utils.buffer").close(bufnr)
-						end)
-					end,
-					desc = "Pick to close",
-				},
-				["<Leader>b"] = { name = "Buffers" },
-				["<Leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
-				["<Leader>re"] = {
-					function()
-						require("nvim-python-repl").send_statement_definition()
-					end,
-					desc = "Send semantic unit to REPL",
-				},
-				["<Leader>B"] = { name = "Bounding Box" },
-				["<Leader>Bb"] = {
-					"<Cmd>CBccbox<CR>",
-					desc = "Title box",
-				},
-				["<Leader>Bt"] = {
-					"<Cmd>CBllline<CR>",
-					desc = "Title line",
-				},
-				["<Leader>Bl"] = {
-					"<Cmd>CBline<CR>",
-					desc = "Comment line",
-				},
-			},
-			t = {
-				-- setting a mapping to false will disable it
-				-- ["<esc>"] = false,
-			},
-			v = {
-				["<Leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
-				["<Leader>re"] = {
-					function()
-						require("nvim-python-repl").send_visual_to_repl()
-					end,
-					desc = "Send visual selection to REPL",
-				},
-				["<Leader>B"] = { name = "Bounding Box" },
-				["<Leader>Bb"] = {
-					"<Cmd>CBccbox<CR>",
-					desc = "Title box",
-				},
-				["<Leader>Bt"] = {
-					"<Cmd>CBllline<CR>",
-					desc = "Title line",
-				},
-				["<Leader>Bl"] = {
-					"<Cmd>CBline<CR>",
-					desc = "Comment line",
-				},
-			},
-		},
-	},
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    -- Configure core features of AstroNvim
+    features = {
+      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      autopairs = true, -- enable autopairs at start
+      cmp = true, -- enable completion at start
+      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+      highlighturl = true, -- highlight URLs at start
+      notifications = true, -- enable notifications at start
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+    diagnostics = {
+      virtual_text = true,
+      underline = true,
+    },
+    -- passed to `vim.filetype.add`
+    filetypes = {
+      -- see `:h vim.filetype.add` for usage
+      extension = {
+        foo = "fooscript",
+      },
+      filename = {
+        [".foorc"] = "fooscript",
+      },
+      pattern = {
+        [".*/etc/foo/.*"] = "fooscript",
+      },
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = false, -- sets vim.opt.spell
+        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+        wrap = false, -- sets vim.opt.wrap
+      },
+      g = { -- vim.g.<key>
+        -- configure global vim variables (vim.g)
+        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+        -- This can be found in the `lua/lazy_setup.lua` file
+      },
+    },
+    -- Mappings can be configured through AstroCore as well.
+    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+    mappings = {
+      n = {
+        -- second key is the lefthand side of the map
+        -- mappings seen under group name "Buffer"
+        --
+        ["-"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
+        ["\\"] = false,
+        ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+        ["<Leader>bD"] = {
+          function()
+            require("astronvim.utils.status").heirline.buffer_picker(
+              function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+            )
+          end,
+          desc = "Pick to close",
+        },
+        ["<Leader>b"] = { name = "Buffers" },
+        ["<Leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
+        ["<Leader>re"] = {
+          function() require("nvim-python-repl").send_statement_definition() end,
+          desc = "Send semantic unit to REPL",
+        },
+        ["<Leader>B"] = { name = "Bounding Box" },
+        ["<Leader>Bb"] = {
+          "<Cmd>CBccbox<CR>",
+          desc = "Title box",
+        },
+        ["<Leader>Bt"] = {
+          "<Cmd>CBllline<CR>",
+          desc = "Title line",
+        },
+        ["<Leader>Bl"] = {
+          "<Cmd>CBline<CR>",
+          desc = "Comment line",
+        },
+      },
+      t = {
+        -- setting a mapping to false will disable it
+        -- ["<esc>"] = false,
+      },
+      v = {
+        ["<Leader>j"] = { "<cmd>HopWord<cr>", desc = "Hop" },
+        ["<Leader>re"] = {
+          function() require("nvim-python-repl").send_visual_to_repl() end,
+          desc = "Send visual selection to REPL",
+        },
+        ["<Leader>B"] = { name = "Bounding Box" },
+        ["<Leader>Bb"] = {
+          "<Cmd>CBccbox<CR>",
+          desc = "Title box",
+        },
+        ["<Leader>Bt"] = {
+          "<Cmd>CBllline<CR>",
+          desc = "Title line",
+        },
+        ["<Leader>Bl"] = {
+          "<Cmd>CBline<CR>",
+          desc = "Comment line",
+        },
+      },
+    },
+
+    -- 	{
+    --   -- first key is the mode
+    --   n = {
+    --     -- second key is the lefthand side of the map
+    --
+    --     -- navigate buffer tabs
+    --     ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+    --     ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+    --
+    --     -- mappings seen under group name "Buffer"
+    --     ["<Leader>bd"] = {
+    --       function()
+    --         require("astroui.status.heirline").buffer_picker(
+    --           function(bufnr) require("astrocore.buffer").close(bufnr) end
+    --         )
+    --       end,
+    --       desc = "Close buffer from tabline",
+    --     },
+    --
+    --     -- tables with just a `desc` key will be registered with which-key if it's installed
+    --     -- this is useful for naming menus
+    --     -- ["<Leader>b"] = { desc = "Buffers" },
+    --
+    --     -- setting a mapping to false will disable it
+    --     -- ["<C-S>"] = false,
+    --   },
+    -- },
+  },
 }
--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,65 +1,62 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- You can also add or configure plugins by creating files in this `plugins/` folder
+-- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
 -- Here are some examples:
 
 ---@type LazySpec
 return {
-
-	-- == Examples of Adding Plugins ==
-
-	"andweeb/presence.nvim",
 	{
 		"ray-x/lsp_signature.nvim",
 		event = "BufRead",
-		config = function()
-			require("lsp_signature").setup()
-		end,
+		config = function() require("lsp_signature").setup() end,
 	},
 
-	-- == Examples of Overriding Plugins ==
-
-	-- customize alpha options
+	-- customize dashboard options
 	{
-		"goolord/alpha-nvim",
-		opts = function(_, opts)
-			-- customize the dashboard header
-			opts.section.header.val = {
-				"                                                                        ",
-				"                                       ,,                               ",
-				"                                     `7MM                               ",
-				"                                       MM                               ",
-				"`7M'   `MF',6\"Yb.  `7MMpMMMb.pMMMb.    MM                               ",
-				"  VA   ,V 8)   MM    MM    MM    MM    MM                               ",
-				"   VA ,V   ,pm9MM    MM    MM    MM    MM                               ",
-				"    VVV   8M   MM    MM    MM    MM    MM                               ",
-				"    ,V    `Moo9^Yo..JMML  JMML  JMML..JMML.                             ",
-				"   ,V                                                                   ",
-				'OOb"                                                                    ',
-				"                               ,,                                       ",
-				"                               db                                       ",
-				"                                                                        ",
-				' .gP"Ya `7MMpMMMb.  .P"Ybmmm `7MM  `7MMpMMMb.  .gP"Ya   .gP"Ya `7Mb,od8',
-				",M'   Yb  MM    MM :MI  I8     MM    MM    MM ,M'   Yb ,M'   Yb  MM' \"' ",
-				'8M""""""  MM    MM  WmmmP"     MM    MM    MM 8M"""""" 8M""""""  MM     ',
-				"YM.    ,  MM    MM 8M          MM    MM    MM YM.    , YM.    ,  MM     ",
-				" `Mbmmd'.JMML  JMML.YMMMMMb  .JMML..JMML  JMML.`Mbmmd'  `Mbmmd'.JMML.   ",
-				"                   6'     dP                                            ",
-				"                   Ybmmmd'                                              ",
-			}
-			return opts
-		end,
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = {
+				preset = {
+
+					header = table.concat({
+						"                                                                        ",
+						"                                       ,,                               ",
+						"                                     `7MM                               ",
+						"                                       MM                               ",
+						"`7M'   `MF',6\"Yb.  `7MMpMMMb.pMMMb.    MM                               ",
+						"  VA   ,V 8)   MM    MM    MM    MM    MM                               ",
+						"   VA ,V   ,pm9MM    MM    MM    MM    MM                               ",
+						"    VVV   8M   MM    MM    MM    MM    MM                               ",
+						"    ,V    `Moo9^Yo..JMML  JMML  JMML..JMML.                             ",
+						"   ,V                                                                   ",
+						'OOb"                                                                    ',
+						"                               ,,                                       ",
+						"                               db                                       ",
+						"                                                                        ",
+						' .gP"Ya `7MMpMMMb.  .P"Ybmmm `7MM  `7MMpMMMb.  .gP"Ya   .gP"Ya `7Mb,od8',
+						",M'   Yb  MM    MM :MI  I8     MM    MM    MM ,M'   Yb ,M'   Yb  MM' \"' ",
+						'8M""""""  MM    MM  WmmmP"     MM    MM    MM 8M"""""" 8M""""""  MM     ',
+						"YM.    ,  MM    MM 8M          MM    MM    MM YM.    , YM.    ,  MM     ",
+						" `Mbmmd'.JMML  JMML.YMMMMMb  .JMML..JMML  JMML.`Mbmmd'  `Mbmmd'.JMML.   ",
+						"                   6'     dP                                            ",
+						"                   Ybmmmd'                                              ",
+					}, "\n"),
+				},
+			},
+		},
 	},
 
 	-- You can disable default plugins as follows:
+	-- { "max397574/better-escape.nvim", enabled = false },
 
 	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
 	{
 		"L3MON4D3/LuaSnip",
 		config = function(plugin, opts)
-			require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
+			require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
 			-- add more custom luasnip configuration such as filetype extend or custom snippets
-			local luasnip = require("luasnip")
+			local luasnip = require "luasnip"
 			luasnip.filetype_extend("javascript", { "javascriptreact" })
 		end,
 	},
@@ -67,16 +64,16 @@ return {
 	{
 		"windwp/nvim-autopairs",
 		config = function(plugin, opts)
-			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
+			require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
 			-- add more custom autopairs configuration such as custom rules
-			local npairs = require("nvim-autopairs")
-			local Rule = require("nvim-autopairs.rule")
-			local cond = require("nvim-autopairs.conds")
+			local npairs = require "nvim-autopairs"
+			local Rule = require "nvim-autopairs.rule"
+			local cond = require "nvim-autopairs.conds"
 			npairs.add_rules(
 				{
 					Rule("$", "$", { "tex", "latex" })
 					-- don't add a pair if the next character is %
-							:with_pair(cond.not_after_regex("%%"))
+							:with_pair(cond.not_after_regex "%%")
 					-- don't add a pair if  the previous character is xxx
 							:with_pair(
 								cond.not_before_regex("xxx", 3)
@@ -84,7 +81,7 @@ return {
 					-- don't move right when repeat character
 							:with_move(cond.none())
 					-- don't delete if the next character is xx
-							:with_del(cond.not_after_regex("xx"))
+							:with_del(cond.not_after_regex "xx")
 					-- disable adding a newline when you press <cr>
 							:with_cr(cond.none()),
 				},
@@ -113,10 +110,10 @@ return {
 		dependencies = "nvim-treesitter",
 		ft = { "python", "lua", "scala" },
 		config = function()
-			require("nvim-python-repl").setup({
+			require("nvim-python-repl").setup {
 				execute_on_send = true,
 				vsplit = true,
-			})
+			}
 		end,
 	},
 	{ "joerdav/templ.vim" },
