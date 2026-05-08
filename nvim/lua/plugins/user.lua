6ce8b3dff25523da2fe0b6612bb0b105b7b2418b
Aditya W <aditya@cube.asia>
2026-05-08 13:06:35 +0700

    .

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -93,8 +93,9 @@ return {
   { "wakatime/vim-wakatime", lazy = false },
   { "pearofducks/ansible-vim", event = "User Astrofile" },
   {
-    "phaazon/hop.nvim",
-    branch = "v2", -- optional but strongly recommended
+    "smoka7/hop.nvim",
+    version = "*",
+    -- branch = "v2", -- optional but strongly recommended
     opts = {
       keys = "wersdfuiojklcmvn",
     },
