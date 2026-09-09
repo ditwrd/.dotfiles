24edf2f491529f2c54628cda29579e1406589fb9
Aditya W <aditya.wardianto11@gmail.com>
2026-09-09 10:05:47 +0700

    .

--- config/alias.nix
+++ config/alias.nix
@@ -56,6 +56,10 @@
     wcc = "warp-cli connect";
     wcd = "warp-cli disconnect";
     wcs="warp-cli status";
+
+    dcc = "export HTTPS_PROXY=http://localhost:1080";
+    dcd = "unset HTTPS_PROXY";
+
     po = "sudo cpupower frequency-set -g powersave && powerprofilesctl set power-saver && sudo ryzenadj --power-saving";
     pb = "sudo cpupower frequency-set -g powersave && powerprofilesctl set balanced";
     pe = "sudo cpupower frequency-set -g performance && powerprofilesctl set performance && sudo ryzenadj --max-performance";
